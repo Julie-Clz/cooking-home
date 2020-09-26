8f221ba7f6f058aebd9fe351d0fb52e4c7b81f97
julie collazos <juliecollazos@hotmail.com>
2020-09-26 18:57:05 +0200

    configure init devise sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :sex, presence: true
+  validates :birth_date, presence: true
+  validates :encrypted_password, presence: true
 end
